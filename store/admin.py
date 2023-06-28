f50ce2f9cd00a07913c1b579a17be51ad366a90a
Roman Smolin <yarosvet@bk.ru>
2023-06-29 00:56:46 +0400

    Третья практика окончена

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Category, Products, Discount
+from .models import Category, Products, Discount, Cart, Profile
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Products)
 admin.site.register(Discount)
+admin.site.register(Cart)
+admin.site.register(Profile)
