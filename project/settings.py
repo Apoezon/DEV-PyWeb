201fbb106d6b6bda003be486f160a0652c74824b
Roman Smolin <yarosvet@bk.ru>
2023-06-29 15:32:04 +0400

    Just before deploy

--- project/settings.py
+++ project/settings.py
@@ -19,18 +19,16 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY') #'django-insecure-i*fm$2pjihuzcy)0xr%4j+#!q7jd!wk^%-4orh=#gft-#5(@y*'
+SECRET_KEY = os.getenv('SECRET_KEY')  # 'django-insecure-i*fm$2pjihuzcy)0xr%4j+#!q7jd!wk^%-4orh=#gft-#5(@y*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == 'true' #True
+DEBUG = os.getenv('DEBUG') == 'true'  # True
 
 ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS').split(',')]
-
 
 # Application definition
 
@@ -77,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -86,17 +83,20 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-'default': {
-   'ENGINE': 'django.db.backends.postgresql',
-   'NAME': os.getenv('NAME_PGDB'),
-   'USER': os.getenv('USER_PGDB'),
-   'PASSWORD': os.getenv('PASSWORD_PGDB'),
-   'HOST': os.getenv('HOST_PGDB'),
-   'PORT': os.getenv('PORT_PGDB'),
-}
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.getenv('NAME_PGDB'),
+    #     'USER': os.getenv('USER_PGDB'),
+    #     'PASSWORD': os.getenv('PASSWORD_PGDB'),
+    #     'HOST': os.getenv('HOST_PGDB'),
+    #     'PORT': os.getenv('PORT_PGDB'),
+    # },
+    'test': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test_db.sqlite3',
+    }
 
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -117,8 +117,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTHENTICATION_BACKENDS = [
-   'store.backends.CustomAuthBackend',
-   'django.contrib.auth.backends.ModelBackend',
+    'store.backends.CustomAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
 ]
 
 # Internationalization
@@ -132,12 +132,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'static')]
+if 'localhost' in ALLOWED_HOSTS:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
